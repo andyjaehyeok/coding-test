8f8024a10ace7340fd03703211a53f9c9bbeac1e
jaehyeokHan <andy.jaehyeok@gmail.com>
2025-12-12 16:27:33 +0900

    [CodingTest] Day 02

--- Level0/index.ts
+++ Level0/index.ts
@@ -22,3 +22,49 @@ function divide(num1: number, num2: number) {
 function divideAndTimes(num1: number, num2: number) {
   return Math.floor((num1 / num2) * 1000);
 }
+
+//숫자 비교하기
+function compare(num1: number, num2: number) {
+  if (num1 === num2) {
+    return 1;
+  } else {
+    return -1;
+  }
+}
+
+//분수의 덧셈
+function fractionSum(
+  num1: number,
+  denom1: number,
+  num2: number,
+  denom2: number
+) {
+  let arr: number[] = [];
+
+  let timeNum1 = num1 * denom2;
+  let timeNum2 = num2 * denom1;
+  let timeDenom2 = denom1 * denom2;
+
+  let sumNum = timeNum1 + timeNum2;
+  let sumDenom = timeDenom2;
+
+  const greatestCommonDivisor = (num1: number, num2: number) => {
+    while (num2 !== 0) {
+      [num1, num2] = [num2, num1 % num2];
+    }
+    return num1;
+  };
+  const num = greatestCommonDivisor(sumNum, sumDenom);
+
+  arr.push(sumNum / num, sumDenom / num);
+  return arr;
+}
+
+//배열 두배 만들기
+function arrDouble(numbers: number[]) {
+  let tempArr: number[] = [];
+  for (let i = 0; i < numbers.length; i++) {
+    tempArr.push(numbers[i] * 2);
+  }
+  return tempArr;
+}

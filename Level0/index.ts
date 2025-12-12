7f6a851c1a8d56080c7411fee229d04e002b2b3a
jaehyeokHan <andy.jaehyeok@gmail.com>
2025-12-12 11:56:01 +0900

    [CodingTest] Day1

---
+++ Level0/index.ts
@@ -0,0 +1,19 @@
+//두 수의 차 구하기
+function minus(num1: number, num2: number) {
+  return num1 - num2;
+}
+
+//두 수의 합 구하기
+function plus(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+//두 수의 곱 구하기
+function times(num1: number, num2: number) {
+  return num1 * num2;
+}
+
+//몫 구하기
+function divide(num1: number, num2: number) {
+  return Math.floor(num1 / num2);
+}

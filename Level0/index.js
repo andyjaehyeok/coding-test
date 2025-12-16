cc4c46c13cab156b44c35f3a61fc70e20fcf6250
andyjaehyeok <andy.jaehyeok@gmail.com>
2025-12-16 23:16:08 +0900

    [CodingTest] Day 05

--- Level0/index.js
+++ Level0/index.js
@@ -3,5 +3,22 @@ function reverseArray(array){
     return array.reverse();
 }
 
+//문자열 뒤집기
+function reverseString(my_string) {
+    let array=[];
+
+    for(let i=my_string.length-1; i>=0; i--){
+        array.push(my_string[i])
+    }
+
+
+    let sum= "";
+    for(let i=0; i<array.length; i++){
+        sum += array[i]
+    }
+
+    return sum;
+}
+
 
 

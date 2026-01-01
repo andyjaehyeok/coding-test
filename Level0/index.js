9c5cc1fbaf87183a8912b0b68fcc21ba6c338f12
andyjaehyeok <andy.jaehyeok@gmail.com>
2026-01-01 21:45:42 +0900

    [CodingTest] level0

--- Level0/index.js
+++ Level0/index.js
@@ -1,1 +1,16 @@
-function test() {}
+function solution(my_string) {
+  let arr = [];
+  const string = my_string.toLowerCase();
+
+  for (let i = 0; i < string.length; i++) {
+    arr.push(string[i]);
+  }
+
+  arr.sort();
+  let sum = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}

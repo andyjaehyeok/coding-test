651f4429f20720e9a88ee7e470dfe771763056b4
andyjaehyeok <andy.jaehyeok@gmail.com>
2026-01-07 22:42:13 +0900

    [CodingTest] Level 0

--- Level0/index.js
+++ Level0/index.js
@@ -1,19 +1,7 @@
-function solution(numbers, direction) {
-  let arr = [];
-  if (direction === "right") {
-    arr.push(numbers[numbers.length - 1]);
-    for (let i = 0; i < numbers.length - 1; i++) {
-      arr.push(numbers[i]);
+function solution(n) {
+  for (let i = 1; i <= n; i++) {
+    if ((i * 6) % n === 0) {
+      return i;
     }
-  } else {
-    for (let i = 1; i < numbers.length; i++) {
-      arr.push(numbers[i]);
-    }
-
-    arr.push(numbers[0]);
   }
-
-  return arr;
 }
-
-console.log(solution([4, 455, 6, 4, -1, 45, 6], "left"));

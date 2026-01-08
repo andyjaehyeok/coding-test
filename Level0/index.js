10b07e2bf9e326fef02726e18c44132973b7923d
andyjaehyeok <andy.jaehyeok@gmail.com>
2026-01-08 23:33:19 +0900

    [CodingTest] Level 0

--- Level0/index.js
+++ Level0/index.js
@@ -1,7 +1,16 @@
-function solution(n) {
-  for (let i = 1; i <= n; i++) {
-    if ((i * 6) % n === 0) {
-      return i;
+function solution(order) {
+  const arr = [];
+  const str = String(order);
+  for (let i = 0; i < str.length; i++) {
+    arr.push(str[i]);
+  }
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "3" || arr[i] === "6" || arr[i] === "9") {
+      count++;
     }
   }
+  return count;
 }
+
+console.log(solution(29423));
